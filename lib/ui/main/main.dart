a2005fbb4dcfdb6b3d7740b6360785b2c25f8a66
Emre <emreergun@bgnmobi.com>
2021-05-22 13:24:31 +0300

    Added new cardview with parameters

--- lib/ui/main/main.dart
+++ lib/ui/main/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,8 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
+        canvasColor: Colors.white,
         primarySwatch: Colors.orange,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -31,35 +35,222 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         backgroundColor: Colors.white,
         leading: IconButton(
           icon: Icon(Icons.menu),
           onPressed: () {},
         ),
-        title: CircleImageAppBar(),
+        actions: appBarActions(),
+        title: buildCircleImage(),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Yunus',
-            ),
+          children: [
+            buildBodyHeader(),
+
+            buildCardview(
+                context,
+                Colors.orange,
+                "Design Highway",
+                "Senior for designers\nana design leads",
+                "22.05.2021",
+                "Fethiye",
+                "15",
+                "Emre\nERGÜN"),
+
+            buildCardview(
+                context,
+                Colors.lightBlue,
+                "Ölüdeniz Beach",
+                "Summer beach\nAll coctails",
+                "25.05.2021",
+                "Dalaman",
+                "Free",
+                "Yunus\nERGÜN"),
+
+            buildCardview(
+                context,
+                Colors.lightGreen,
+                "Design Highway",
+                "Senior for designers\nana design leads",
+                "22.05.2021",
+                "Fethiye",
+                "15",
+                "Emre\nERGÜN"),
+
+            buildCardview(
+                context,
+                Colors.purple,
+                "Design Highway",
+                "Senior for designers\nana design leads",
+                "22.05.2021",
+                "Fethiye",
+                "15",
+                "Emre\nERGÜN"),
+
 
           ],
         ),
       ),
     );
   }
-}
 
-class CircleImageAppBar extends StatelessWidget {
-  const CircleImageAppBar({
-    Key? key,
-  }) : super(key: key);
+  Center buildCardview(BuildContext context,MaterialColor materialColor, String header, String subHeader,
+      String date, String location, String price, String cardName) {
+    return Center(
+      child: Container(
+        margin: EdgeInsets.only(top: 10, bottom: 10),
+        height: MediaQuery.of(context).size.height * 0.40,
+        width: MediaQuery.of(context).size.width * 0.9,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: Colors.orange,
+            gradient: LinearGradient(
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
+                colors: [materialColor.shade900, materialColor.shade300])),
+        child: Stack(
+          children: [
+            Positioned(
+              top: 20,
+              left: 20,
+              child: Text(
+                header,
+                style:
+                    GoogleFonts.nunitoSans(fontSize: 40, color: Colors.white),
+              ),
+            ),
+            Positioned(
+              top: 70,
+              left: 20,
+              child: Text(
+                subHeader,
+                style: GoogleFonts.nunitoSans(
+                  fontSize: 20,
+                  color: Colors.white70,
+                  height: 0.9,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 30,
+              left: 20,
+              child: Text(
+                "Location : $location",
+                style: GoogleFonts.nunitoSans(
+                  color: Colors.white70,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 60,
+              left: 20,
+              child: Text(
+                "Date : $date",
+                style: GoogleFonts.nunitoSans(
+                  color: Colors.white70,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: Container(
+                alignment: Alignment.center,
+                height: 70,
+                width: 70,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(60),
+                    color: materialColor.shade700),
+                child: Text(
+                  "\$ $price",
+                  style: GoogleFonts.nunitoSans(
+                    color: Colors.white70,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 90,
+              right: 20,
+              child: Text(
+                cardName,
+                textAlign: TextAlign.end,
+                style: GoogleFonts.nunitoSans(
+                    color: Colors.white10, fontSize: 70, height: 1),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
-  @override
-  Widget build(BuildContext context) {
+  // < Back Professional events
+  Container buildBodyHeader() {
+    return Container(
+      margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
+      child: Row(
+        children: [
+          Row(
+            children: [
+              IconButton(onPressed: () {}, icon: Icon(Icons.chevron_left)),
+              Text(
+                "Back",
+                style: GoogleFonts.lato(
+                  fontSize: 22,
+                  fontWeight: FontWeight.normal,
+                ),
+              )
+            ],
+          ),
+          Container(
+            width: 50,
+          ),
+          Text(
+            "Professional events",
+            style: GoogleFonts.lato(
+              fontSize: 22,
+              fontWeight: FontWeight.normal,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  // Actions
+  List<Widget> appBarActions() {
+    return [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "California",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.keyboard_arrow_down_rounded),
+          )
+        ],
+      ),
+      IconButton(
+        onPressed: () {},
+        icon: Icon(Icons.search),
+      ),
+      Container(
+        width: 10,
+      )
+    ];
+  }
+
+  // Circle image
+  ClipRRect buildCircleImage() {
     return ClipRRect(
       borderRadius: BorderRadius.circular(30),
       child: Stack(
@@ -68,9 +259,7 @@ class CircleImageAppBar extends StatelessWidget {
           Container(
             height: 40,
             width: 40,
-            decoration: BoxDecoration(
-              color: Colors.lightBlue
-            ),
+            decoration: BoxDecoration(color: Colors.lightBlue),
           ),
           ClipRRect(
             borderRadius: BorderRadius.circular(35),
